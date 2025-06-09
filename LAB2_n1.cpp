9e7c057cd9f519f8ac97c5ba4cf076e8f5a764a3
Brain17 <b.daminov@newuu.uz>
2025-06-09 18:47:19 +0500

    Add files via upload

---
+++ LAB2_n1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  int a;
+    cin>>a;
+    if (a>0) {
+        cout<< "Positive" << endl;
+    } else if (a==0) {
+        cout<< "Neither positive nor negative" << endl;
+        }
+    else {
+        cout<< "Negative" << endl;
+    }
+
+
+    return 0;
+}
